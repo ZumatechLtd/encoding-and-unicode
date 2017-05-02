6447315afcd56aae4fe8cf5eb3608385d7b5d51f
Jordan Powell <jordan.rhys.powell@gmail.com>
2017-05-02 18:44:43 +0100

    Fix typo

--- python3/encoding_decoding.py
+++ python3/encoding_decoding.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
-    print(b'\xe2\x89\x83'.decode('utf-8'))
-    print(b'\xe2\x89\x83'.decode())
+    print(b'\xe2\x98\x83'.decode('utf-8'))
+    print(b'\xe2\x98\x83'.decode())
     print('\u2603'.encode('utf-8'))
